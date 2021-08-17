24f2cb50c39f99ae9cbc0eb8b8d867614dc77b1c
Moein zargarzadeh <moein.zargarzadeh@gmail.com>
2021-08-17 08:57:32 +0430

    Added production configuration

--- rasta/asgi.py
+++ rasta/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rasta.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rasta.settings.development')
 
 application = get_asgi_application()
