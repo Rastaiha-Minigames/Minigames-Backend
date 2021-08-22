f82844ebef00e7b8eb9d700350e24d394f03e9b6
Danny <d.khorasanizadeh@ec.iut.ac.ir>
2021-08-22 16:49:55 +0430

    Image Workshop Utility Functions

---
+++ imageWorkshop/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ImageworkshopConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'imageWorkshop'
---
+++ imageWorkshop/utilities.py
@@ -0,0 +1,30 @@
+import re
+import cv2
+import base64
+import numpy as np
+FRACTION_REGEX = re.compile(r'^([-+]?\d+)\/?(\d*)$')
+
+
+def convertToFloat(fractionString):
+    try:
+        return float(fractionString)
+    except ValueError:
+        if FRACTION_REGEX.match(fractionString):
+            numerator, denomerator = FRACTION_REGEX.match(
+                fractionString).groups()
+            if denomerator is None:
+                return int(numerator)
+            return int(numerator)/int(denomerator)
+        return 0
+
+
+def dataURItoCV2Image(data):
+    nparray = np.fromstring(data.decode('base64'), np.uint8)
+    image = cv2.imdecode(nparray, cv2.IMREAD_COLOR)
+    return image
+
+
+def CV2ImageToDataURI(image):
+    _, encoded_img = cv2.imencode('.jpg', image)
+    data = base64.b64encode(encoded_img).decode("utf-8")
+    return data
