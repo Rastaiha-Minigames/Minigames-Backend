4b85950142d52424f1cfaec10b7a661f7cc6bd64
f.labbaf98 <f.labbaf98@gmail.com>
2021-08-26 17:18:42 +0430

    filter and time mini-game

--- frequencyWorkshop/urls.py
+++ frequencyWorkshop/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from frequencyWorkshop.views import soundFilter, fftView
+from frequencyWorkshop.views import soundFilter, fftView, timeView
 
 app_name='TeamData'
 urlpatterns = [
     path('fft/', fftView, name='fft'),
+    path('timeplot/', timeView, name='fft'),
     path('filter/', soundFilter, name='filter')
 ]
--- frequencyWorkshop/views.py
+++ frequencyWorkshop/views.py
@@ -13,6 +13,10 @@ import librosa as librosa
 from scipy.fftpack import fft
 import numpy as np
 
+import librosa as librosa
+from scipy.io import wavfile as wav
+from scipy.signal import butter, lfilter
+from .utils import *
 INPUT_SOUNDS_DIR = 'media/frequencyWorkshop/input_sounds/'
 OUTPUT_SOUNDS_DIR = 'media/frequencyWorkshop/output_sounds/'
 FFT_DIR = 'media/frequencyWorkshop/FFT/'
@@ -34,15 +38,12 @@ def fftView(request):
         duration = None
 
     file = INPUT_SOUNDS_DIR + request.data['sound_file']
-    print(file)
 
     data, rate = librosa.load(file, offset=start, duration=duration)
     samples_num = len(data)
 
-    print(samples_num)
-
     fft_out = fft(data)
-
+    plt.clf()
     x_range = [k * rate / samples_num for k in range(0, (samples_num // 2))]
     plt.plot(x_range, np.abs(fft_out[-1*(samples_num // 2):]))
 
@@ -56,22 +57,72 @@ def fftView(request):
     return Response({'fft_dir': BASE_DIR + fft_dir})
 
 
-
 @transaction.atomic
 @api_view(['POST'])
 def soundFilter(request):
     if 'sound_file' not in request.data:
         raise ParseError("Empty content")
-    file_name = request.data['sound_file']
+    file_name = INPUT_SOUNDS_DIR + request.data['sound_file']
 
     if 'lowcut' in request.data:
         lowcut = float(request.data['lowcut'])
     else:
-        lowcut = 0.0
+        lowcut = 50
     if 'highcut' in request.data:
         highcut = float(request.data['highcut'])
     else:
-        highcut = 10 #TODO change to the end of sound
+        highcut = 1000 #TODO change to the end of sound
 
-    return Response({'success': True}, status=status.HTTP_200_OK)
+    data, rate = librosa.load(file_name)
+    samples_num = len(data)
 
+    plt.clf()
+    x = fft(data)
+    x_range = [k * rate / samples_num for k in range(0, (samples_num // 2))]
+    plt.plot(x_range, np.abs(x[-1*(samples_num // 2):])/samples_num, label='Noisy signal')
+
+    y = butter_bandpass_filter(data, lowcut, highcut, 6000, order=9)
+    fft_out = fft(y)
+    plt.plot(x_range, np.abs(fft_out[-1 * (samples_num // 2):])/samples_num, label='Filtered signal')
+
+    plt.grid(True)
+    plt.legend(loc='upper left')
+
+    name = ''.join(random.choice(string.ascii_letters) for i in range(7))
+    fft_dir = FFT_DIR + name + '.png'
+    plt.savefig(fft_dir)
+
+    sound_name = ''.join(random.choice(string.ascii_letters) for i in range(7))
+    sound_dir = OUTPUT_SOUNDS_DIR + sound_name + '.wav'
+    wav.write(sound_dir, rate, y)
+
+    return Response({'fft_dir': BASE_DIR + fft_dir, 'sound_dir': BASE_DIR + sound_dir})
+
+
+@transaction.atomic
+@api_view(['POST'])
+def timeView(request):
+    if 'sound_file' not in request.data:
+        raise ParseError("Empty content")
+    if 'start' in request.data:
+        start = float(request.data['start'])
+    else:
+        start = 0.0
+    if 'end' in request.data:
+        duration = float(request.data['end'])-start
+    else:
+        duration = None
+
+    file = INPUT_SOUNDS_DIR + request.data['sound_file']
+
+    data, rate = librosa.load(file, offset=start, duration=duration)
+    samples_num = len(data)
+    plt.clf()
+    x_range = [k/rate for k in range(0, samples_num)]
+    plt.plot(x_range, data)
+
+    name = ''.join(random.choice(string.ascii_letters) for i in range(8))
+    fft_dir = FFT_DIR + name + '.png'
+    plt.savefig(FFT_DIR + name+'.png')
+
+    return Response({'fft_dir': BASE_DIR + fft_dir})
