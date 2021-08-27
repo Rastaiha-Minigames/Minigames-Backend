dccb56172cf40a1ddfa0535fa3d6efc8f946c5f9
f.labbaf98 <f.labbaf98@gmail.com>
2021-08-27 19:40:54 +0430

    edit frequency apis

--- frequencyWorkshop/urls.py
+++ frequencyWorkshop/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 
 from frequencyWorkshop.views import soundFilter, fftView, timeView
 
-app_name='TeamData'
+app_name='frequencyWorkshop'
 urlpatterns = [
     path('fft/', fftView, name='fft'),
-    path('timeplot/', timeView, name='fft'),
+    path('timeplot/', timeView, name='timeplot'),
     path('filter/', soundFilter, name='filter')
-]
+]   
--- frequencyWorkshop/views.py
+++ frequencyWorkshop/views.py
@@ -1,11 +1,11 @@
 import string
 import random
+from datetime import datetime
 
 from django.db import transaction
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework.exceptions import ParseError
 import matplotlib.pyplot as plt
 import librosa as librosa
@@ -13,10 +13,8 @@ import librosa as librosa
 from scipy.fftpack import fft
 import numpy as np
 
-import librosa as librosa
-from scipy.io import wavfile as wav
-from scipy.signal import butter, lfilter
 from .utils import *
+
 INPUT_SOUNDS_DIR = 'media/frequencyWorkshop/input_sounds/'
 OUTPUT_SOUNDS_DIR = 'media/frequencyWorkshop/output_sounds/'
 FFT_DIR = 'media/frequencyWorkshop/FFT/'
@@ -47,7 +45,8 @@ def fftView(request):
     x_range = [k * rate / samples_num for k in range(0, (samples_num // 2))]
     plt.plot(x_range, np.abs(fft_out[-1*(samples_num // 2):]))
 
-    name = ''.join(random.choice(string.ascii_letters) for i in range(6))
+    t = str(datetime.now().strftime('%H:%M-%S'))
+    name = t + ''.join(random.choice(string.ascii_letters) for i in range(8))
     fft_dir = FFT_DIR + name + '.png'
     plt.savefig(FFT_DIR + name+'.png')
 
@@ -64,6 +63,15 @@ def soundFilter(request):
         raise ParseError("Empty content")
     file_name = INPUT_SOUNDS_DIR + request.data['sound_file']
 
+    if 'start' in request.data:
+        start = float(request.data['start'])
+    else:
+        start = 0.0
+    if 'end' in request.data:
+        duration = float(request.data['end'])-start
+    else:
+        duration = None
+
     if 'lowcut' in request.data:
         lowcut = float(request.data['lowcut'])
     else:
@@ -71,32 +79,46 @@ def soundFilter(request):
     if 'highcut' in request.data:
         highcut = float(request.data['highcut'])
     else:
-        highcut = 1000 #TODO change to the end of sound
+        highcut = 3000 #TODO change to the end of sound
 
-    data, rate = librosa.load(file_name)
+    data, rate = librosa.load(file_name, offset=start, duration=duration)
     samples_num = len(data)
+
+    t = str(datetime.now().strftime('%H:%M-%S'))
+    name = t + ''.join(random.choice(string.ascii_letters) for i in range(8))
 
     plt.clf()
     x = fft(data)
     x_range = [k * rate / samples_num for k in range(0, (samples_num // 2))]
     plt.plot(x_range, np.abs(x[-1*(samples_num // 2):])/samples_num, label='Noisy signal')
+    fft_dir = FFT_DIR + 'fft' + name + '.png'
+    plt.savefig(fft_dir)
 
     y = butter_bandpass_filter(data, lowcut, highcut, 6000, order=9)
     fft_out = fft(y)
+    plt.clf()
     plt.plot(x_range, np.abs(fft_out[-1 * (samples_num // 2):])/samples_num, label='Filtered signal')
+    filtered_fft_dir = FFT_DIR + 'ffft' + name + '.png'
+    plt.savefig(filtered_fft_dir)
 
-    plt.grid(True)
-    plt.legend(loc='upper left')
+    plt.clf()
+    x_range = [k / rate for k in range(0, samples_num)]
+    plt.plot(x_range, y)
+    time_dir = FFT_DIR + 'time' + name + '.png'
+    plt.savefig(time_dir)
 
-    name = ''.join(random.choice(string.ascii_letters) for i in range(7))
-    fft_dir = FFT_DIR + name + '.png'
-    plt.savefig(fft_dir)
+    # plt.grid(True)
+    # plt.legend(loc='upper left')
+
 
     sound_name = ''.join(random.choice(string.ascii_letters) for i in range(7))
     sound_dir = OUTPUT_SOUNDS_DIR + sound_name + '.wav'
     wav.write(sound_dir, rate, y)
 
-    return Response({'fft_dir': BASE_DIR + fft_dir, 'sound_dir': BASE_DIR + sound_dir})
+    return Response({'fft_dir': BASE_DIR + fft_dir,
+                     'filtered_fft_dir': BASE_DIR + filtered_fft_dir,
+                     'filtered_time_dir': BASE_DIR + time_dir,
+                     'sound_dir': BASE_DIR + sound_dir})
 
 
 @transaction.atomic
@@ -121,8 +143,9 @@ def timeView(request):
     x_range = [k/rate for k in range(0, samples_num)]
     plt.plot(x_range, data)
 
-    name = ''.join(random.choice(string.ascii_letters) for i in range(8))
+    t = str(datetime.now().strftime('%H:%M-%S'))
+    name = t + ''.join(random.choice(string.ascii_letters) for i in range(8))
     fft_dir = FFT_DIR + name + '.png'
     plt.savefig(FFT_DIR + name+'.png')
 
-    return Response({'fft_dir': BASE_DIR + fft_dir})
+    return Response({'time_dir': BASE_DIR + fft_dir})
