2e7c940fbf2f0ae48d4743d9b93ea0ba049c885d
f.labbaf98 <f.labbaf98@gmail.com>
2021-08-30 04:55:57 +0430

    morphology games

--- imageWorkshop/urls.py
+++ imageWorkshop/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from imageWorkshop.views import threshold, channels, filter_im
+from imageWorkshop.views import *
 
 app_name ='imageWorkshop'
 urlpatterns = [
     path('threshold/', threshold, name='password'),
     path('filter/', filter_im, name='filter'),
     path('channels/', channels, name='channels'),
-    # path('blending/', blending, name='blending'),
+    path('morpho2/', channels_mask, name='morpho2'),
+    path('morpho3/', dilate_erode, name='morpho3'),
+    path('morpho4/', morpho_final, name='morpho4'),
 ]
--- imageWorkshop/views.py
+++ imageWorkshop/views.py
@@ -16,7 +16,6 @@ import matplotlib.pyplot as plt
 import cv2
 import json
 
-from .utilities import *
 
 INPUT_IMAGE_DIR = 'media/imageWorkshop/input_images/'
 OUTPUT_IMAGE_DIR = 'media/imageWorkshop/output_images/'
@@ -92,6 +91,7 @@ def channels(request):
         file_dir = INPUT_IMAGE_DIR + request.data['image_file']
 
     im = cv2.imread(file_dir)
+    im = cv2.resize(im, (600, 400))
 
     # extract channels
     # G_channel = np.dstack((image[:, :, 1], image[:, :, 1], image[:, :, 1]))
@@ -116,3 +116,136 @@ def channels(request):
     return Response({'blue_image_dir': BASE_DIR + blue_image_dir,
                      'green_image_dir': BASE_DIR + green_image_dir,
                      'red_image_dir': BASE_DIR + red_image_dir})
+
+
+@transaction.atomic
+@api_view(['POST'])
+def channels_mask(request):
+    channels_code = {'G': 1, 'B': 0, 'R': 2}
+    if 'image_file' not in request.data:
+        file_dir = INPUT_IMAGE_DIR +'bar255.jpg'
+    else:
+        file_dir = INPUT_IMAGE_DIR + request.data['image_file']
+    if 'color' not in request.data:
+        color = 'G'
+        c = channels_code[color]
+    else:
+        color = request.data['color']
+        c = channels_code[color]
+    if 'threshold' not in request.data:
+        raise ParseError("Empty content")
+    tr = int(request.data['threshold'])
+
+    im = cv2.imread(file_dir)
+    im = cv2.resize(im, (600, 400))
+    channel = np.dstack((im[:, :, c], im[:, :, c], im[:, :, c]))
+    ret, mask = cv2.threshold(channel, tr, 255, cv2.THRESH_BINARY)
+
+
+    t = str(datetime.now().strftime('%H:%M-%S'))
+    name = t + ''.join(random.choice(string.ascii_letters) for i in range(8))
+    image_dir = OUTPUT_IMAGE_DIR + 'morpho2' + name + '.png'
+
+    # write channels to greyscale image
+    plt.clf()
+    plt.imsave(arr=mask, fname=image_dir)
+    plt.close()
+
+    return Response({'image_dir': BASE_DIR + image_dir})
+
+
+@transaction.atomic
+@api_view(['POST'])
+def dilate_erode(request):
+    channels_code = {'G': 1, 'B': 0, 'R': 2}
+    if 'image_file' not in request.data:
+        file_dir = INPUT_IMAGE_DIR +'bar255.jpg'
+    else:
+        file_dir = INPUT_IMAGE_DIR + request.data['image_file']
+    if 'color' not in request.data:
+        color = 'G'
+        c = channels_code[color]
+    else:
+        color = request.data['color']
+        c = channels_code[color]
+    if 'threshold' not in request.data:
+        raise ParseError("Empty content")
+    tr = int(request.data['threshold'])
+    if 'method' not in request.data:
+        raise ParseError("Empty content")
+    method = request.data['method']
+    if 'size' not in request.data:
+        raise ParseError("Empty content")
+    sizeSE = int(request.data['size'])
+
+    im = cv2.imread(file_dir)
+    im = cv2.resize(im, (600, 400))
+    channel = np.dstack((im[:, :, c], im[:, :, c], im[:, :, c]))
+    ret, mask = cv2.threshold(channel, tr, 255, cv2.THRESH_BINARY)
+
+    SE = np.ones((sizeSE, sizeSE))
+    if method == 'dilate':
+        result = cv2.dilate(mask, SE)
+    else:
+        result = cv2.erode(mask, SE)
+
+    t = str(datetime.now().strftime('%H:%M-%S'))
+    name = t + ''.join(random.choice(string.ascii_letters) for i in range(8))
+    image_dir = OUTPUT_IMAGE_DIR + name + '.png'
+
+    # write channels to greyscale image
+    plt.clf()
+    plt.imsave(arr=result, fname=image_dir)
+
+    return Response({'image_dir': BASE_DIR + image_dir})
+
+
+@transaction.atomic
+@api_view(['POST'])
+def morpho_final(request):
+    if 'image_file' not in request.data:
+        file_dir = INPUT_IMAGE_DIR +'bar255.jpg'
+    else:
+        file_dir = INPUT_IMAGE_DIR + request.data['image_file']
+    if 'mask_file' not in request.data:
+        mask_dir = OUTPUT_IMAGE_DIR +'bar255.jpg'
+    else:
+        mask_dir = request.data['mask_file']
+        mask_dir = mask_dir[mask_dir.find('media/'):]
+    if 'method' not in request.data:
+        raise ParseError("Empty content")
+    method = request.data['method']
+    if 'size' not in request.data:
+        raise ParseError("Empty content")
+    sizeSE = int(request.data['size'])
+
+    image = cv2.imread(file_dir)
+    image = cv2.resize(image, (600, 400))
+
+    mask = cv2.imread(mask_dir)
+    mask = cv2.resize(mask, (600, 400))
+
+    SE = np.ones((sizeSE, sizeSE))
+    if method == 'dilate':
+        mainMask = cv2.dilate(mask, SE)
+    else:
+        mainMask = cv2.erode(mask, SE)
+
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            if mainMask[i, j, 1]:
+                image[i, j, 1] = 255
+
+    t = str(datetime.now().strftime('%H:%M-%S'))
+    name = t + ''.join(random.choice(string.ascii_letters) for i in range(8))
+
+    image_dir = OUTPUT_IMAGE_DIR + name + '.png'
+    plt.clf()
+    plt.imsave(arr=cv2.cvtColor(image, cv2.COLOR_BGR2RGB), fname=image_dir)
+    
+    plt.clf()
+    mask_dir = OUTPUT_IMAGE_DIR + 'mask' + name + '.png'
+    plt.imsave(arr=mainMask, fname=mask_dir)
+
+    return Response({'image_dir': BASE_DIR + image_dir,
+                     'mask_dir': BASE_DIR + mask_dir})
